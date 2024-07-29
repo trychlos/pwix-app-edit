59606fed8d06da61b6b6a0684ce33bb8f8dd91d9
Pierre Wieser <p.wieser@trychlos.org>
2024-07-30 00:27:28 +0200

    Releasing v 1.0.0

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'pwix:app-edit',
-    version: '1.0.0-rc',
+    version: '1.0.0',
     summary: 'Extends a Meteor RunContext to let an application have editable inline documents',
     git: 'https://github.com/trychlos/pwix-app-edit.git',
     documentation: 'README.md'
